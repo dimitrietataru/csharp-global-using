a955a1e5fbc40062bf4d0d7aa309df49741e3ff9
Dimitrie Tataru <dimitrie.tataru.bp@emea.nttdata.com>
2022-09-17 00:09:46 +0300

    Add Microsoft.AspNetCore.* usings

---
+++ sample/GlobalUsing.Sample.Api/Globals.Microsoft.AspNetCore.All.cs
@@ -0,0 +1,160 @@
+#pragma warning disable IDE0005 // Using directive is unnecessary
+
+global using Microsoft.AspNetCore;
+
+global using Microsoft.AspNetCore.Antiforgery;
+
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authentication.Cookies;
+global using Microsoft.AspNetCore.Authentication.OAuth;
+global using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Authorization.Infrastructure;
+global using Microsoft.AspNetCore.Authorization.Policy;
+
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Builder.Extensions;
+
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Authorization;
+global using Microsoft.AspNetCore.Components.CompilerServices;
+global using Microsoft.AspNetCore.Components.Forms;
+global using Microsoft.AspNetCore.Components.Infrastructure;
+global using Microsoft.AspNetCore.Components.Rendering;
+global using Microsoft.AspNetCore.Components.RenderTree;
+global using Microsoft.AspNetCore.Components.Routing;
+global using Microsoft.AspNetCore.Components.Server;
+global using Microsoft.AspNetCore.Components.Server.Circuits;
+global using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.Web.Infrastructure;
+global using Microsoft.AspNetCore.Components.Web.Virtualization;
+
+global using Microsoft.AspNetCore.Connections;
+global using Microsoft.AspNetCore.Connections.Features;
+
+global using Microsoft.AspNetCore.CookiePolicy;
+
+global using Microsoft.AspNetCore.Cors;
+global using Microsoft.AspNetCore.Cors.Infrastructure;
+
+global using Microsoft.AspNetCore.Cryptography;
+global using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+global using Microsoft.AspNetCore.DataProtection;
+global using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+global using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
+global using Microsoft.AspNetCore.DataProtection.Infrastructure;
+global using Microsoft.AspNetCore.DataProtection.Internal;
+global using Microsoft.AspNetCore.DataProtection.KeyManagement;
+global using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
+global using Microsoft.AspNetCore.DataProtection.Repositories;
+global using Microsoft.AspNetCore.DataProtection.XmlEncryption;
+
+global using Microsoft.AspNetCore.Diagnostics;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+global using Microsoft.AspNetCore.HostFiltering;
+
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Hosting.Builder;
+global using Microsoft.AspNetCore.Hosting.Infrastructure;
+global using Microsoft.AspNetCore.Hosting.Server;
+global using Microsoft.AspNetCore.Hosting.Server.Abstractions;
+global using Microsoft.AspNetCore.Hosting.Server.Features;
+global using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+
+global using Microsoft.AspNetCore.Html;
+
+global using Microsoft.AspNetCore.Http;
+
+global using Microsoft.AspNetCore.HttpLogging;
+
+global using Microsoft.AspNetCore.HttpOverrides;
+
+global using Microsoft.AspNetCore.HttpsPolicy;
+
+global using Microsoft.AspNetCore.Identity;
+
+global using Microsoft.AspNetCore.Localization;
+global using Microsoft.AspNetCore.Localization.Routing;
+
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.Abstractions;
+global using Microsoft.AspNetCore.Mvc.ActionConstraints;
+global using Microsoft.AspNetCore.Mvc.ApiExplorer;
+global using Microsoft.AspNetCore.Mvc.ApplicationModels;
+global using Microsoft.AspNetCore.Mvc.ApplicationParts;
+global using Microsoft.AspNetCore.Mvc.Authorization;
+global using Microsoft.AspNetCore.Mvc.Controllers;
+global using Microsoft.AspNetCore.Mvc.Core;
+global using Microsoft.AspNetCore.Mvc.Core.Infrastructure;
+global using Microsoft.AspNetCore.Mvc.Localization;
+global using Microsoft.AspNetCore.Mvc.ModelBinding;
+global using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+global using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+global using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+global using Microsoft.AspNetCore.Mvc.Razor;
+global using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+global using Microsoft.AspNetCore.Mvc.Razor.Infrastructure;
+global using Microsoft.AspNetCore.Mvc.Razor.Internal;
+global using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+global using Microsoft.AspNetCore.Mvc.Rendering;
+global using Microsoft.AspNetCore.Mvc.Routing;
+global using Microsoft.AspNetCore.Mvc.TagHelpers;
+global using Microsoft.AspNetCore.Mvc.TagHelpers.Cache;
+global using Microsoft.AspNetCore.Mvc.ViewComponents;
+global using Microsoft.AspNetCore.Mvc.ViewEngines;
+global using Microsoft.AspNetCore.Mvc.ViewFeatures;
+global using Microsoft.AspNetCore.Mvc.ViewFeatures.Buffers;
+global using Microsoft.AspNetCore.Mvc.ViewFeatures.Infrastructure;
+
+global using Microsoft.AspNetCore.Razor;
+global using Microsoft.AspNetCore.Razor.Hosting;
+global using Microsoft.AspNetCore.Razor.Runtime;
+global using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+global using Microsoft.AspNetCore.Razor.TagHelpers;
+
+global using Microsoft.AspNetCore.ResponseCaching;
+
+global using Microsoft.AspNetCore.ResponseCompression;
+
+global using Microsoft.AspNetCore.Rewrite;
+
+global using Microsoft.AspNetCore.Routing;
+global using Microsoft.AspNetCore.Routing.Constraints;
+global using Microsoft.AspNetCore.Routing.Internal;
+global using Microsoft.AspNetCore.Routing.Matching;
+global using Microsoft.AspNetCore.Routing.Patterns;
+global using Microsoft.AspNetCore.Routing.Template;
+global using Microsoft.AspNetCore.Routing.Tree;
+
+global using Microsoft.AspNetCore.Server;
+global using Microsoft.AspNetCore.Server.HttpSys;
+global using Microsoft.AspNetCore.Server.IIS;
+global using Microsoft.AspNetCore.Server.IIS.Core;
+global using Microsoft.AspNetCore.Server.IISIntegration;
+global using Microsoft.AspNetCore.Server.Kestrel;
+global using Microsoft.AspNetCore.Server.Kestrel.Core;
+global using Microsoft.AspNetCore.Server.Kestrel.Core.Features;
+global using Microsoft.AspNetCore.Server.Kestrel.Core.Internal;
+global using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+global using Microsoft.AspNetCore.Server.Kestrel.Https;
+global using Microsoft.AspNetCore.Server.Kestrel.Transport;
+global using Microsoft.AspNetCore.Server.Kestrel.Transport.Quic;
+global using Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets;
+
+global using Microsoft.AspNetCore.Session;
+
+global using Microsoft.AspNetCore.SignalR;
+global using Microsoft.AspNetCore.SignalR.Protocol;
+
+global using Microsoft.AspNetCore.StaticFiles;
+global using Microsoft.AspNetCore.StaticFiles.Infrastructure;
+
+global using Microsoft.AspNetCore.WebSockets;
+
+global using Microsoft.AspNetCore.WebUtilities;
---
+++ sample/GlobalUsing.Sample.Api/Globals.Microsoft.AspNetCore.cs
@@ -0,0 +1,16 @@
+#pragma warning disable IDE0005 // Using directive is unnecessary
+
+global using Microsoft.AspNetCore;
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Cors.Infrastructure;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Localization;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Rewrite;
+global using Microsoft.AspNetCore.Routing;
+global using Microsoft.AspNetCore.SignalR;
