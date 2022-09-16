5705723a0e164957815eacdf6f36ae8ec157b6cc
Dimitrie Tataru <dimitrie.tataru.bp@emea.nttdata.com>
2022-09-17 00:10:00 +0300

    Add Microsoft.Extensions.* usings

---
+++ sample/GlobalUsing.Sample.Api/Globals.Microsoft.Extensions.All.cs
@@ -0,0 +1,64 @@
+#pragma warning disable IDE0005 // Using directive is unnecessary
+
+global using Microsoft.Extensions;
+
+global using Microsoft.Extensions.Caching;
+global using Microsoft.Extensions.Caching.Distributed;
+global using Microsoft.Extensions.Caching.Memory;
+
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.Configuration.CommandLine;
+global using Microsoft.Extensions.Configuration.EnvironmentVariables;
+global using Microsoft.Extensions.Configuration.Ini;
+global using Microsoft.Extensions.Configuration.Json;
+global using Microsoft.Extensions.Configuration.KeyPerFile;
+global using Microsoft.Extensions.Configuration.Memory;
+global using Microsoft.Extensions.Configuration.UserSecrets;
+global using Microsoft.Extensions.Configuration.Xml;
+
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.DependencyInjection.Extensions;
+
+global using Microsoft.Extensions.Diagnostics;
+global using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+global using Microsoft.Extensions.FileProviders;
+global using Microsoft.Extensions.FileProviders.Composite;
+global using Microsoft.Extensions.FileProviders.Embedded;
+global using Microsoft.Extensions.FileProviders.Internal;
+global using Microsoft.Extensions.FileProviders.Physical;
+
+global using Microsoft.Extensions.FileSystemGlobbing;
+global using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
+global using Microsoft.Extensions.FileSystemGlobbing.Internal;
+global using Microsoft.Extensions.FileSystemGlobbing.Internal.PathSegments;
+global using Microsoft.Extensions.FileSystemGlobbing.Internal.PatternContexts;
+global using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Hosting.Internal;
+
+global using Microsoft.Extensions.Http;
+global using Microsoft.Extensions.Http.Logging;
+
+global using Microsoft.Extensions.Internal;
+
+global using Microsoft.Extensions.Localization;
+
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Logging.Abstractions;
+global using Microsoft.Extensions.Logging.Configuration;
+global using Microsoft.Extensions.Logging.Console;
+global using Microsoft.Extensions.Logging.Debug;
+global using Microsoft.Extensions.Logging.EventLog;
+global using Microsoft.Extensions.Logging.EventSource;
+global using Microsoft.Extensions.Logging.TraceSource;
+
+global using Microsoft.Extensions.ObjectPool;
+
+global using Microsoft.Extensions.Options;
+
+global using Microsoft.Extensions.Primitives;
+
+global using Microsoft.Extensions.WebEncoders;
+global using Microsoft.Extensions.WebEncoders.Testing;
---
+++ sample/GlobalUsing.Sample.Api/Globals.Microsoft.Extensions.cs
@@ -0,0 +1,14 @@
+#pragma warning disable IDE0005 // Using directive is unnecessary
+
+global using Microsoft.Extensions;
+global using Microsoft.Extensions.Caching.Memory;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.DependencyInjection.Extensions;
+global using Microsoft.Extensions.Diagnostics.HealthChecks;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Http;
+global using Microsoft.Extensions.Localization;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Options;
+global using Microsoft.Extensions.Primitives;
